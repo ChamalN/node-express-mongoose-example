643cadb849a4d38276ffe396ad9f9b5bcf764c70
ChamalN <chamal.yash@gmail.com>
2016-04-06 15:08:21 +0530

    Add delete route

--- routes/students.js
+++ routes/students.js
@@ -17,7 +17,6 @@ router.get('/', function(req, res) {
 
 // create new student
 router.post('/', function(req, res) {
-  console.log(req.body);
   var student = new Student(req.body);
   student.save(function(err, student) {
     if(err) {
@@ -32,6 +31,18 @@ router.post('/', function(req, res) {
 // get student by id
 router.get('/:studentId', function(req, res) {
   Student.findOne({ studentId: req.params.studentId }, 'studentId firstName lastName', function(err, student) {
+    if(err) {
+      console.log("Could not retrieve student: " + err);
+      res.json(err);
+    } else {
+      res.json(student);
+    }
+  });
+});
+
+// delete student by id
+router.delete('/:studentId', function(req, res) {
+  Student.remove({ studentId: req.params.studentId }, function(err, student) {
     if(err) {
       console.log("Could not retrieve student: " + err);
       res.json(err);
