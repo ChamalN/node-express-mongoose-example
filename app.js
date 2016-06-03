5174ee052c201fefa1a31ba6f32a3367fb3b9d08
ChamalN <chamal.yash@gmail.com>
2016-06-03 14:51:53 +0530

    Added status route for testing

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var db = require('./db');
+//var db = require('./db');
 
-var students = require('./routes/students');
+//var students = require('./routes/students');
 
 var app = express();
 
 app.use(bodyParser.json());
-app.use('/students', students);
+//app.use('/students', students);
+
+app.get('/status', function (req, res) {
+  res.json({"status":"ok"});
+});
 
 app.listen(8080, function() {
   console.log('App started on port 8080...')
