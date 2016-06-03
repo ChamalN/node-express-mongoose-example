9425ad82d6377860919e1591b257089d14b107e1
ChamalN <chamal.yash@gmail.com>
2016-06-03 15:00:09 +0530

    changed port to 80

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-//var db = require('./db');
-
-//var students = require('./routes/students');
-
 var app = express();
 
 app.use(bodyParser.json());
-//app.use('/students', students);
 
 app.get('/status', function (req, res) {
   res.json({"status":"ok"});
 });
 
-app.listen(8080, function() {
+app.listen(80, function() {
   console.log('App started on port 8080...')
 });
